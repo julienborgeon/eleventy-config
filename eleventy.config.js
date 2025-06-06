2502337d78b3ac07f7cccb3c9b486cabb8b3116d
Julien Borgeon <julienborgeon@gmail.com>
2025-06-06 12:58:54 +0200

    Complete eleventy config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,14 +1,25 @@
 import { DateTime } from "luxon";
 
-export default async function (eleventyConfig) {
+export default function (eleventyConfig) {
+	// Date format filter
 	eleventyConfig.addFilter("postDate", (dateObj) =>
 		DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
 	);
+
+	// Copy static files to output folder
+	eleventyConfig.addPassthroughCopy({ "./src/assets": "assets" });
+	eleventyConfig.addPassthroughCopy({ "./src/css": "css" });
 
 	return {
 		dir: {
 			input: "src",
 			output: "public",
+			includes: "_includes",
+			layouts: "_layouts",
+			data: "_data",
 		},
+		markdownTemplateEngine: "njk",
+		htmlTemplateEngine: "njk",
+		templateFormats: ["md", "njk", "html"],
 	};
 }
